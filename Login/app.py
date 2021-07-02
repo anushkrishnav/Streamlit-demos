3aea9eb0ced0f49a8663e25571e75d192030598d
A N U S H <54374648+anushkrishnav@users.noreply.github.com>
2021-07-02 09:27:09 +0530

    Update app.py

--- Login/app.py
+++ Login/app.py
@@ -1,1 +1,22 @@
 import streamlit as st
+from streamlit import config
+from src.utils.state import SessionState
+
+def write_page(page):  # pylint: disable=redefined-outer-name
+    return page.write()
+ 
+PAGES={ 'Home':home, 'About':intro, 'Stocks':dash}
+
+def main():
+    choice=st.sidebar.radio("Click on the pages to explore",tuple(PAGES.keys()))
+
+    if choice ==None:
+        write_page(dash) 
+    else:
+        page=PAGES[choice]
+        with st.spinner(f"Loading {choice} ..."):
+            write_page(page)
+
+
+if __name__ == "__main__":
+        main()
